facc4ccbf0233f9663361efb27dfa52462f3ebb2
Gurminder Singh <gurmindersingh@Gurminders-MacBook-Pro.local>
2022-12-13 16:53:38 -0500

    Added deleted files

---
+++ src/Calculator.Services/ServiceRegistry.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ServiceRegistry.cs" company="Sohi"
+// Copyright (c) Sohi. All rights reserved.
+// </copyright>
+
+namespace Calculator.Services
+{
+    using Calculator.Abstractions.Services;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Register the necessary dependency injection items for the calculator.
+    /// </summary>
+    public static class ServiceRegistry
+    {
+        /// <summary>
+        /// Configure the calculator services.
+        /// </summary>
+        /// <param name="services">A <see cref="IServiceCollection"/> to add the calculator services to.</param>
+        /// <param name="configuration">A <see cref="IConfiguration"/> to add the calculator services to.</param>
+        /// <returns>Service collection.</returns>
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //configuration.NotNull(nameof(configuration));
+
+            services.AddTransient(typeof(ILoanService), typeof(LoanService));
+
+            return services;
+        }
+    }
+}
+
---
+++ test/Calculator.Services.Tests/MortgageServiceTests.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="MortgageServiceTests.cs" company="Sohi"
+// Copyright (c) Sohi. All rights reserved.
+// </copyright>
+
+namespace Calculator.Services.Tests
+{
+    using System.Security.Principal;
+    using Calculator.DataTransferModels.Enums;
+    using Calculator.DataTransferModels.Loan;
+    using Calculator.DataTransferModels.Mortgage;
+    using FluentAssertions;
+    using FluentAssertions.Common;
+
+    /// <summary>
+    /// Test Case for mortgage calculator service.
+    /// </summary>
+	public class MortgageServiceTests
+    {
+        private readonly MortgageService _service;
+
+        public MortgageServiceTests()
+        {
+            _service = new MortgageService();
+        }
+
+        [Theory]
+        [InlineData(39176.00, 7, 0, PaymentFrequency.Monthly, 3.79, 531.71, 65.33, 5487.68, 44663.68)]
+        [InlineData(300000, 25, 0, PaymentFrequency.Monthly, 1, 1130.62, 130.62, 39185.21, 339185.21)]
+        [InlineData(300000, 25, 6, PaymentFrequency.Monthly, 1, 1111.11, 130.71, 39998.17, 339998.17)]
+        [InlineData(39176.00, 7, 0, PaymentFrequency.SemiMonthly, 3.79, 265.67, 32.48, 5456.95, 44632.95)]
+        [InlineData(300000, 25, 0, PaymentFrequency.SemiMonthly, 1, 565.21, 65.21, 39123.05, 339123.05)]
+        [InlineData(39176.00, 7, 0, PaymentFrequency.BiWeekly, 3.79, 245.22, 29.97, 5454.59, 44630.59)]
+        [InlineData(300000, 25, 0, PaymentFrequency.BiWeekly, 1, 521.72, 60.18, 39118.26, 339118.26)]
+        [InlineData(39176.00, 7, 0, PaymentFrequency.Weekly, 3.79, 122.57, 14.95, 5440.4, 44616.4)]
+        [InlineData(300000, 25, 0, PaymentFrequency.Weekly, 1, 260.84, 30.07, 39089.57, 339089.57)]
+        public void CalculateLoan_ValidInput_ShouldSucceed(double mortgageAmount,
+             int years,
+             int months,
+             PaymentFrequency paymentFrequency,
+             double interestRate,
+             double expectedMonthlyPayment,
+             double expectedMonthlyInterest,
+             double expectedTotalInterest,
+             double expectedTotalAmount)
+        {
+            // Arrange
+            var item = new MortgageDto()
+            {
+                MortgageAmount = mortgageAmount,
+                Years = years,
+                Months = months,
+                PaymentFrequency = paymentFrequency,
+                InterestRate = interestRate,
+            };
+
+            // Act
+            var response = _service.CalculateMortgage(item);
+
+            // Assert
+            response.Success.Should().Be(true);
+            response.ResponseDto.MonthlyPayment.Should().Be(expectedMonthlyPayment);
+            response.ResponseDto.MonthlyInterest.Should().Be(expectedMonthlyInterest);
+            response.ResponseDto.TotalInterest.Should().Be(expectedTotalInterest);
+            response.ResponseDto.TotalAmount.Should().Be(expectedTotalAmount);
+        }
+    }
+}
+
